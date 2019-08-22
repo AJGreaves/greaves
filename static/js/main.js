0ac34eb30dd45680f05a4eeac34fb3418d5fe125
Anna Greaves <gilhespy.anna@gmail.com>
2019-08-22 17:44:08 +0200

    Add JS to fade out navbar on scroll.

--- static/js/main.js
+++ static/js/main.js
@@ -36,3 +36,15 @@ $('.work-history-btn').click(function() {
     };
     return;
 })
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $(".navbar").addClass('invisible');
+    } else {
+        $(".navbar").removeClass('invisible');
+    }
+}
