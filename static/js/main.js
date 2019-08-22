1fcfac932ec6aa88f5839e9aca67943fab055a0f
Anna Greaves <gilhespy.anna@gmail.com>
2019-08-22 20:42:45 +0200

    Wire up email and set to refresh page after sucessful sending.

--- static/js/main.js
+++ static/js/main.js
@@ -1,50 +1,85 @@
-// code credit: https://bootstrapious.com/p/circular-progress-bar
-$(function () {
+$(document).ready(function () {
 
-    $(".progress").each(function () {
 
-        var value = $(this).attr('data-value');
-        var left = $(this).find('.progress-left .progress-bar');
-        var right = $(this).find('.progress-right .progress-bar');
+    const contactForm = document.querySelector('#contact-form');
 
-        if (value > 0) {
-            if (value <= 50) {
-                right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-            } else {
-                right.css('transform', 'rotate(180deg)')
-                left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const data = {
+            service_id: "gmail",
+            template_id: "resume",
+            user_id: "user_CQSk7h9Wyuw2xLhYG0hBX",
+            template_params: {
+                "name": contactForm.name.value,
+                "email": contactForm.email.value,
+                "message": contactForm.message.value
             }
+        };
+
+        $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json'
+        }).done(function () {
+            $('input').val('');
+            $('textarea').val('');
+            alert('Your email has been sent successfully!');
+            window.location.reload(true); 
+        }).fail(function (error) {
+            console.log('Oops... ' + JSON.stringify(error));
+            alert('Oops something went wrong, please try again');
+        });
+    });
+
+    // code credit: https://bootstrapious.com/p/circular-progress-bar
+    $(function () {
+
+        $(".progress").each(function () {
+
+            var value = $(this).attr('data-value');
+            var left = $(this).find('.progress-left .progress-bar');
+            var right = $(this).find('.progress-right .progress-bar');
+
+            if (value > 0) {
+                if (value <= 50) {
+                    right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+                } else {
+                    right.css('transform', 'rotate(180deg)')
+                    left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+                }
+            }
+
+        })
+
+        function percentageToDegrees(percentage) {
+
+            return percentage / 100 * 360
+
         }
 
+    });
+
+    // Changes readmore button text to close when element is open.
+    $('.work-history-btn').click(function () {
+        let text = $(this).text();
+        if (text == 'Close') {
+            $(this).text('Read More')
+        } else {
+            $(this).text('Close')
+        };
+        return;
     })
 
-    function percentageToDegrees(percentage) {
+    window.onscroll = function () {
+        scrollFunction();
+    };
 
-        return percentage / 100 * 360
-
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            $(".navbar").addClass('invisible');
+        } else {
+            $(".navbar").removeClass('invisible');
+        }
     }
 
 });
-
-// Changes readmore button text to close when element is open.
-$('.work-history-btn').click(function() {
-    let text = $(this).text();
-    if (text ==  'Close') {
-        $(this).text('Read More')
-    } else {
-        $(this).text('Close')
-    };
-    return;
-})
-
-window.onscroll = function () {
-    scrollFunction();
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        $(".navbar").addClass('invisible');
-    } else {
-        $(".navbar").removeClass('invisible');
-    }
-}
