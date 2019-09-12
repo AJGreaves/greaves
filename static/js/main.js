2c66ff25c06b369ce1c6290c5f1991aa34ca29f4
Anna Greaves <gilhespy.anna@gmail.com>
2019-09-12 18:41:42 +0200

    Remove invisible navbar JS and mack white backgrounds off-white.

--- static/js/main.js
+++ static/js/main.js
@@ -70,16 +70,4 @@ $(document).ready(function () {
         return;
     })
 
-    window.onscroll = function () {
-        scrollFunction();
-    };
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            $(".navbar").addClass('invisible');
-        } else {
-            $(".navbar").removeClass('invisible');
-        }
-    }
-
 });
